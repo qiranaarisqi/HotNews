ca3b4e75423fc279ef761cae99cfd08dc8b99242
qiranaarisqi <qiranarahmawati9@gmail.com>
2026-03-19 21:28:11 +0700

    fix bug on navbar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -81,7 +81,7 @@
                     @else
                         <a href="{{ route('login') }}" class="flex items-center space-x-2 group">
                             <svg class="w-6 h-6 text-gray-900 group-hover:text-brand transition" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
-                            <span class="text-sm font-bold text-gray-900 group-hover:text-brand transition">Username</span>
+                            <span class="text-sm font-bold text-gray-900 group-hover:text-brand transition">Sign In</span>
                         </a>
                     @endauth
                 </div>
