965bddfc832f70976917a6479afffbbb44907499
qiranaarisqi <qiranarahmawati9@gmail.com>
2026-03-19 19:58:13 +0700

    fix: force https in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,24 +1,21 @@
 <?php
-
 namespace App\Providers;
-
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+
         if (!app()->runningInConsole()) {
             view()->share('globalCategories', \App\Models\Kategori::all());
             view()->share('globalLatestNews', \App\Models\Article::where('status', 'published')->latest()->take(5)->get());
